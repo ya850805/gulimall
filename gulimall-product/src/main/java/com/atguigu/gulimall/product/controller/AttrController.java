73024f23735ce86cb8d0415665f6d1ccb422c06e
Jason <ya850805@gmail.com>
2021-09-27 22:22:06 +0800

    feat: Add vo(value object fill with front-end data) and insert method with relational table.

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.atguigu.gulimall.product.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,8 +56,10 @@ public class AttrController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody AttrEntity attr){
-		attrService.save(attr);
+    public R save(@RequestBody /*AttrEntity attr*/ AttrVo attrVo){
+//		attrService.save(attr);
+
+        attrService.saveAttr(attrVo);
 
         return R.ok();
     }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.atguigu.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.atguigu.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.atguigu.gulimall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,10 +16,14 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.AttrDao;
 import com.atguigu.gulimall.product.entity.AttrEntity;
 import com.atguigu.gulimall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Autowired
+    private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +33,21 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    @Transactional
+    public void saveAttr(AttrVo attrVo) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attrVo, attrEntity);
+        //1. 保存基本數據
+        this.save(attrEntity);
+
+        //2. 保存關聯關係
+        AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+        attrAttrgroupRelationEntity.setAttrGroupId(attrVo.getAttrGroupId());
+        attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
+        attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
     }
 
 }
