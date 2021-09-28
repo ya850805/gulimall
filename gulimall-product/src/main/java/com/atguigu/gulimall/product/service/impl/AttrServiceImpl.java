767c83a6eb4a544a1353eb1f7fcb6d8fd2500e7f
Jason <ya850805@gmail.com>
2021-09-28 22:22:39 +0800

    feat: modify search response with AttrRespVo(extends from AttrVo and have catelogName & groupName attribute which search from relational table).

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,14 +1,21 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import com.atguigu.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.atguigu.gulimall.product.dao.AttrGroupDao;
+import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.atguigu.gulimall.product.entity.AttrGroupEntity;
+import com.atguigu.gulimall.product.entity.CategoryEntity;
+import com.atguigu.gulimall.product.vo.AttrRespVo;
 import com.atguigu.gulimall.product.vo.AttrVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -27,6 +34,12 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
 
     @Autowired
     private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+
+    @Autowired
+    private AttrGroupDao attrGroupDao;
+
+    @Autowired
+    private CategoryDao categoryDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -73,7 +86,29 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
                 queryWrapper
         );
 
-        return new PageUtils(page);
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrRespVo> respVos = records.stream().map((attrEntity) -> {
+            AttrRespVo attrRespVo = new AttrRespVo();
+            BeanUtils.copyProperties(attrEntity, attrRespVo);
+            //設置分類和分組的名稱
+            AttrAttrgroupRelationEntity attrId =
+                    attrAttrgroupRelationDao.selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrEntity.getAttrId()));
+
+            if (attrId != null) {
+                AttrGroupEntity attrGroupEntity = attrGroupDao.selectById(attrId);
+                attrRespVo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+
+            CategoryEntity categoryEntity = categoryDao.selectById(attrEntity.getCatelogId());
+            if (categoryEntity != null) {
+                attrRespVo.setCatelogName(categoryEntity.getName());
+            }
+            return attrRespVo;
+        }).collect(Collectors.toList());
+
+        pageUtils.setList(respVos);
+        return pageUtils;
     }
 
 }
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/AttrRespVo.java
@@ -0,0 +1,12 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author Jason
+ */
+@Data
+public class AttrRespVo extends AttrVo {
+    private String catelogName;
+    private String groupName;
+}
