93a122ad708bacad05b49a073afe61609bbf85a2
Jason <ya850805@gmail.com>
2021-10-06 22:10:35 +0800

    feat: Add CategoryBrandRelation code.

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/CategoryBrandRelationDao.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.product.dao;
+
+import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 品牌分类关联
+ *
+ * @author Jason
+ */
+@Mapper
+public interface CategoryBrandRelationDao extends BaseMapper<CategoryBrandRelationEntity> {
+
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/CategoryBrandRelationService.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.product.service;
+
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.gulimall.product.entity.CategoryBrandRelationEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+/**
+ * 品牌分类关联
+ *
+ * @author Jason
+ */
+public interface CategoryBrandRelationService extends IService<CategoryBrandRelationEntity> {
+    PageUtils queryPage(Map<String, Object> params);
+}
