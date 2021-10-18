44c2a35e1507cc7748de15cf58f873b1c194a06c
ya850805 <43848699+ya850805@users.noreply.github.com>
2021-10-18 22:28:58 +0800

    docs: Add comment for CouponFeignService.

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/feign/CouponFeignService.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/feign/CouponFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 /**
- * @author Jason
+ * feign service to call gulimall-coupon micro service
  */
 @FeignClient("gulimall-coupon")
 public interface CouponFeignService {
