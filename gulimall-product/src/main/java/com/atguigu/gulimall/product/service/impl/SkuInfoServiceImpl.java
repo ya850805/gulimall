4370ba2d8c2530db83706cb435486800da79eed2
Jason <ya850805@gmail.com>
2021-10-21 22:15:16 +0800

    feat: Add an API that search skuInfo with condition.

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -31,4 +35,42 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
         this.baseMapper.insert(skuInfoEntity);
     }
 
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (StringUtils.isNotBlank(key)) {
+            queryWrapper.and((wrapper) -> {
+                wrapper.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+        String catelogId = (String) params.get("catelogId");
+        if (StringUtils.isNotBlank(catelogId) && !"0".equalsIgnoreCase(catelogId)) {
+            queryWrapper.eq("catalogId", catelogId);
+        }
+        String brandId = (String) params.get("brandId");
+        if (StringUtils.isNotBlank(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+        String min = (String) params.get("min");
+        if (StringUtils.isNotBlank(min)) {
+            queryWrapper.ge("price", min);
+        }
+        String max = (String) params.get("max");
+        if (StringUtils.isNotBlank(max)) {
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if (bigDecimal.compareTo(BigDecimal.ZERO) == 1) {
+                    queryWrapper.le("price", max);
+                }
+            } catch (Exception exception) {
+            }
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                queryWrapper
+        );
+        return new PageUtils(page);
+    }
 }
