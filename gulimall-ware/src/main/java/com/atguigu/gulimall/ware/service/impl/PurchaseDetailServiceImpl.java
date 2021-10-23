97bcd205553394ddfb95a34a10a55754b0c1a848
Jason <ya850805@gmail.com>
2021-10-23 13:32:46 +0800

    feat: Add an API that search purchaseDetail with condition.

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,28 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if(StringUtils.isNotBlank(key)) {
+            queryWrapper.and((wrapper) -> {
+                wrapper.eq("sku_id", key).or().eq("purchase_id", key);
+            });
+        }
+
+        String status = (String) params.get("status");
+        if(StringUtils.isNotBlank(status)) {
+            queryWrapper.eq("status", status);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(StringUtils.isNotBlank(wareId)) {
+            queryWrapper.eq("ware_id", wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
